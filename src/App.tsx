773ff461b53640e214a4a9f1eab872e9a6c9bfab
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-02 00:38:31 +0700

    DIsplay sentence

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
+import { onMount } from "solid-js";
 import type { AnkiFields } from "./types";
 
 function App(props: { ankiFields: AnkiFields }) {
+	let sentenceEl: HTMLDivElement | undefined;
+	onMount(() => {
+		if (sentenceEl) {
+			const ruby = sentenceEl.querySelectorAll("ruby");
+			ruby.forEach((el) => {
+				el.classList.add(..."[&_rt]:invisible hover:[&_rt]:visible".split(" "));
+			});
+		}
+	});
+
 	return (
-		<div class="max-w-4xl mx-auto">
+		<div class="max-w-4xl mx-auto flex flex-col gap-8">
 			<div class="flex rounded-lg gap-4 h-56">
 				<div class="flex-1 bg-base-200 rounded-lg flex flex-col items-center justify-center">
+					<div
+						class="text-3xl"
+						innerHTML={props.ankiFields["kana:ExpressionFurigana"]}
+					></div>
 					<div class="text-6xl" innerHTML={props.ankiFields.Expression}></div>
+					<div class="text-3xl">{/* TODO: pitch  */}</div>
 				</div>
 				<div
 					class="[&_>_img]:h-full [&_>_img]:rounded-lg"
 					innerHTML={props.ankiFields.Picture}
+				></div>
+			</div>
+			<div class="flex flex-col gap-4 items-center text-center">
+				<div
+					class="text-4xl"
+					ref={sentenceEl}
+					innerHTML={
+						props.ankiFields["furigana:SentenceFurigana"] ??
+						props.ankiFields["furigana:Sentence"]
+					}
 				></div>
 			</div>
 		</div>
