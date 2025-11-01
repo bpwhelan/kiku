e3ffb3eb6bd7d407a22aadf352d0ccd3fc418be7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-01 21:11:19 +0700

    Add daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import type { AnkiFields } from "./types";
+
+function App(props: { ankiFields: AnkiFields }) {
 	return (
-		<div class="min-h-screen">
-			<h1 class="text-4xl font-bold underline text-blue-600">
-				Hello world from Shadow DOM!
-			</h1>
+		<div class="bg-base-200" data-theme="coffee">
+			<button class="btn">test</button>
+			<div class="flex">
+				<div class="flex-1 flex flex-col items-center justify-center">
+					<div class="text-6xl" innerHTML={props.ankiFields.Expression}></div>
+				</div>
+				<div class="[&_>_img]:h-64" innerHTML={props.ankiFields.Picture}></div>
+			</div>
 		</div>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App.tsx";
+import "./tailwind.css";
 import tailwind from "./tailwind.css?inline";
+
+import { exampleFields } from "./types.ts";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root not found");
 const shadow = root.attachShadow({ mode: "closed" });
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(tailwind.replaceAll(":root", ":host"));
+shadow.adoptedStyleSheets = [sheet];
 
-const style = document.createElement("style");
-style.textContent = tailwind;
-shadow.appendChild(style);
-render(() => <App />, shadow);
+render(() => <App ankiFields={exampleFields} />, shadow);
+
+document.documentElement.setAttribute("data-theme", "coffee");
