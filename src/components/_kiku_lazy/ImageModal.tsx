aeacd77a48852c3cf6ba71bd0221b05a37bb3681
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-08 00:58:34 +0700

    feat: smooth modal

--- src/components/_kiku_lazy/ImageModal.tsx
+++ src/components/_kiku_lazy/ImageModal.tsx
@@ -1,11 +1,33 @@
+import { createEffect, createSignal } from "solid-js";
+
 export default function ImageModal(props: {
   img: string | undefined;
   "on:click"?: () => void;
 }) {
+  const [showModal, setShowModal] = createSignal(false);
+  const [transparent, setTransparent] = createSignal(true);
+
+  let id: number;
+  createEffect(() => {
+    if (id) clearTimeout(id);
+    if (props.img) {
+      setShowModal(true);
+      setTimeout(() => {
+        setTransparent(false);
+      }, 0);
+    } else {
+      setTransparent(true);
+      id = setTimeout(() => {
+        setShowModal(!!props.img);
+      }, 200);
+    }
+  });
+
   return (
     <div
       class="picture-modal"
-      data-modal-hidden={props.img ? "false" : "true"}
+      data-modal-hidden={showModal() ? "false" : "true"}
+      data-modal-transparent={transparent() ? "true" : "false"}
       on:click={props["on:click"]}
       innerHTML={props.img}
     ></div>
