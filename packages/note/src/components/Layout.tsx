6c4fc8b5eaa597fb42c7a3c006a99ff22dd36733
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-11-13 02:13:41 +0700

    feat: auto height anki web

--- packages/note/src/components/Layout.tsx
+++ packages/note/src/components/Layout.tsx
@@ -1,12 +1,42 @@
-import type { JSX } from "solid-js";
+import { createSignal, type JSX, onMount } from "solid-js";
 import { useCardStore } from "./shared/Context";
+
+let counter = 0;
 
 export function Layout(props: { children: JSX.Element }) {
   const [card] = useCardStore();
   if (card.nested) return props.children;
+  const [layoutElement, setLayoutElement] = createSignal<HTMLDivElement>();
+  const [offset, setOffset] = createSignal(0);
+
+  if (KIKU_STATE.isAnkiWeb) {
+    onMount(() => {
+      const el = layoutElement();
+      if (!el) return;
+      function autoResize() {
+        if (!el) return;
+        const html = window.document.documentElement;
+        const offset = html.scrollHeight - html.clientHeight;
+        if (offset === 0) return;
+        counter = counter + 1;
+        if (counter > 100) return;
+        setOffset(offset);
+      }
+      new ResizeObserver(autoResize).observe(el);
+      autoResize();
+    });
+  }
 
   return (
-    <div class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-svh font-primary">
+    <div
+      ref={setLayoutElement}
+      class="max-w-4xl mx-auto overflow-auto p-2 sm:p-4 gutter-stable h-[calc(100vh-1rem)] sm:h-svh font-primary"
+      style={{
+        height: KIKU_STATE.isAnkiWeb
+          ? `calc(100svh - ${offset()}px)`
+          : undefined,
+      }}
+    >
       <div class="flex flex-col gap-6 ">{props.children}</div>
     </div>
   );
